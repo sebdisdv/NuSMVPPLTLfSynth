f39440904aea780fdba696a6a9dd2760247f9c1f
sebdisdv <sebastiano.dissegna@gmail.com>
2023-09-25 21:09:18 +0200

    added test from folder

--- Pltl2Nusmv/test.py
+++ Pltl2Nusmv/test.py
@@ -2,18 +2,18 @@ from parser import PLTL2NuSmv
 
 import os
 import subprocess
-
+import argparse
 
 NUSMV_EXEC_PATH = "NuSMV/build/bin/NuSMV"
 
 command = lambda fsmv : f"-int -dynamic -source Pltl2Nusmv/nusmv_commands.txt {fsmv}"
 
-def create_NuSMV_files():
-    for _,_,inputs in os.walk("Pltl2Nusmv/input_test"):
+def create_NuSMV_files(folder):
+    for _,_,inputs in os.walk(folder):
         outputs = []
         for i in inputs:
             try:
-                p = PLTL2NuSmv(f"Pltl2Nusmv/input_test/{i}", os.path.basename(i)[:-4])
+                p = PLTL2NuSmv(f"{folder}/{i}", os.path.basename(i)[:-4])
                 outputs.append((p.formula_str, p.write_nusmv()))
             except ValueError:
                 pass
@@ -26,8 +26,8 @@ def execute_file(f):
         return None
     return out
 
-def main():
-    outputs = create_NuSMV_files()
+def main(settings):
+    outputs = create_NuSMV_files(settings.input)
     for output in outputs:
         execution = execute_file(output[1])
         if execution is not None:
@@ -35,4 +35,14 @@ def main():
             print("/"*30, "\n")
         
 if __name__ == "__main__":
-    main()
+    argsparser = argparse.ArgumentParser()
+    argsparser.add_argument(
+        "-i",
+        "--input",
+        default=None,
+        required=True,
+        type=str,
+        help="The folder containig the formulas",
+    )
+    
+    main(settings=argsparser.parse_args())
