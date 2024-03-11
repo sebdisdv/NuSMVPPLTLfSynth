14b0e98f6eba2a4dc88bcb5ea66a8892dd20ae45
sebdisdv <seba.dissegna@gmail.com>
2024-03-11 13:44:21 +0100

    added pattern and mapping to future for generator

--- FormulaGenerator/generator.py
+++ FormulaGenerator/generator.py
@@ -2,6 +2,7 @@ import json
 import random
 import os
 
+
 from typing import List
 from argparse import ArgumentParser
 
@@ -12,11 +13,15 @@ class PLTL:
         return p
 
     @staticmethod
+    def neg(p: str) -> str:
+        return f"! {p}"
+
+    @staticmethod
     def yesterday(p: str) -> str:
         return f"(Y {p})"
 
     @staticmethod
-    def once(p: str) -> str:
+    def existence(p: str) -> str:
         return f"(O {p})"
 
     @staticmethod
@@ -44,11 +49,270 @@ class PLTL:
         return f"(O ({p1} & Y({formula})))"
 
     @staticmethod
+    def persistence(p: str) -> str:
+        return f"H (O ({p}))"
+
+    # up to here
+
+    @staticmethod
+    def abscence(p: str) -> str:
+        return f"!O({p})"
+
+    @staticmethod
+    def choice(p1: str, p2: str) -> str:
+        return f"O({p1}) | O({p2})"
+
+    @staticmethod
+    def exclusive_choice(p1: str, p2: str) -> str:
+        return f"(O({p1}) | O({p2})) &  !(O({p1}) | O({p2}))"
+
+    @staticmethod
+    def responded_existence(p1: str, p2: str) -> str:
+        return f"O({p1}) -> O({p2})"
+
+    @staticmethod
+    def co_existence(p1: str, p2: str) -> str:
+        return f"H(!({p1})) <-> H(!({p2}))"
+
+    @staticmethod
+    def response(p1: str, p2: str) -> str:
+        return f"(!({p1}) S ({p2})) | H (! ({p1}))"
+
+    @staticmethod
+    def precedence(p1: str, p2: str) -> str:
+        return f"H(({p2}) -> O({p1}))"
+
+    @staticmethod
+    def chain_response(p1: str, p2: str) -> str:
+        return f"H(Y({p1}) -> ({p2})) & !({p1})"
+
+    @staticmethod
+    def chain_precedence(p1: str, p2: str) -> str:
+        return f"H(({p2}) -> Y({p1}))"
+
+    @staticmethod
+    def chain_succession(p1: str, p2: str) -> str:
+        return f"(H(Y({p1}) -> ({p2})) & !({p1})) & H(Y(!({p1})) -> !({p2}))"
+
+    @staticmethod
+    def not_co_existence(p1: str, p2: str) -> str:
+        return f"O({p1}) -> !O({p2})"
+
+    @staticmethod
+    def not_succession(p1: str, p2: str) -> str:
+        return f"H(({p2}) -> !O({p1}))"
+
+    @staticmethod
+    def not_chain_succession(p1: str, p2: str) -> str:
+        return f"H(({p2}) -> !Y({p1}))"
+
+    @staticmethod
     def get_op_dict() -> dict:
         return {
-            "unary": [PLTL.yesterday, PLTL.once, PLTL.historically, PLTL.atom],
-            "binary": [PLTL.land, PLTL.lor, PLTL.implies, PLTL.seq, PLTL.since],
+            "unary": [
+                PLTL.yesterday,
+                PLTL.existence,
+                PLTL.historically,
+                PLTL.atom,
+                PLTL.neg,
+                PLTL.persistence,
+                PLTL.abscence,
+            ],
+            "binary": [
+                PLTL.land,
+                PLTL.lor,
+                PLTL.implies,
+                PLTL.seq,
+                PLTL.since,
+                PLTL.choice,
+                PLTL.exclusive_choice,
+                PLTL.co_existence,
+                PLTL.response,
+                PLTL.chain_response,
+                PLTL.chain_precedence,
+                PLTL.chain_succession,
+                PLTL.not_co_existence,
+                PLTL.not_chain_succession,
+                PLTL.not_succession,
+            ],
         }
+
+
+class PLTL_pattern:
+
+    @staticmethod
+    def existence(p: str) -> str:
+        return f"(O {p})"
+
+    @staticmethod
+    def abscence(p: str) -> str:
+        return f"!O({p})"
+
+    @staticmethod
+    def choice(p1: str, p2: str) -> str:
+        return f"O({p1}) | O({p2})"
+
+    @staticmethod
+    def exclusive_choice(p1: str, p2: str) -> str:
+        return f"(O({p1}) | O({p2})) &  !(O({p1}) | O({p2}))"
+
+    @staticmethod
+    def co_existence(p1: str, p2: str) -> str:
+        return f"H(!({p1})) <-> H(!({p2}))"
+
+    @staticmethod
+    def responded_existence(p1: str, p2: str) -> str:
+        return f"O({p1}) -> O({p2})"
+
+    @staticmethod
+    def response(p1: str, p2: str) -> str:
+        return f"(!({p1}) S ({p2})) | H (! ({p1}))"
+
+    @staticmethod
+    def precedence(p1: str, p2: str) -> str:
+        return f"H(({p2}) -> O({p1}))"
+
+    @staticmethod
+    def chain_response(p1: str, p2: str) -> str:
+        return f"H(Y({p1}) -> ({p2})) & !({p1})"
+
+    @staticmethod
+    def chain_precedence(p1: str, p2: str) -> str:
+        return f"H(({p2}) -> Y({p1}))"
+
+    @staticmethod
+    def chain_succession(p1: str, p2: str) -> str:
+        return f"(H(Y({p1}) -> ({p2})) & !({p1})) & H(Y(!({p1})) -> !({p2}))"
+
+    @staticmethod
+    def not_co_existence(p1: str, p2: str) -> str:
+        return f"O({p1}) -> !O({p2})"
+
+    @staticmethod
+    def not_succession(p1: str, p2: str) -> str:
+        return f"H(({p2}) -> !O({p1}))"
+
+    @staticmethod
+    def not_chain_succession(p1: str, p2: str) -> str:
+        return f"H(({p2}) -> !Y({p1}))"
+
+    @staticmethod
+    def get_op_dict() -> dict:
+        return {
+            "unary": [PLTL_pattern.existence, PLTL_pattern.abscence],
+            "binary": [
+                PLTL_pattern.choice,
+                PLTL_pattern.exclusive_choice,
+                PLTL_pattern.co_existence,
+                PLTL_pattern.responded_existence,
+                PLTL_pattern.response,
+                PLTL_pattern.precedence,
+                PLTL_pattern.chain_response,
+                PLTL_pattern.chain_precedence,
+                PLTL_pattern.chain_succession,
+                PLTL_pattern.not_co_existence,
+                PLTL_pattern.not_succession,
+                PLTL_pattern.not_chain_succession,
+            ],
+        }
+
+
+class LTLf_pattern:
+
+    @staticmethod
+    def existence(p: str) -> str:
+        return f"(O {p})"
+
+    @staticmethod
+    def abscence(p: str) -> str:
+        return f"!F({p})"
+
+    @staticmethod
+    def choice(p1: str, p2: str) -> str:
+        return f"F({p1}) | F({p2})"
+
+    @staticmethod
+    def exclusive_choice(p1: str, p2: str) -> str:
+        return f"(F({p1}) | F({p2})) &  !(F({p1}) | F({p2}))"
+
+    @staticmethod
+    def co_existence(p1: str, p2: str) -> str:
+        return f"F({p1}) <-> F({p2})"
+
+    @staticmethod
+    def responded_existence(p1: str, p2: str) -> str:
+        return f"F({p1}) -> F({p2})"
+
+    @staticmethod
+    def response(p1: str, p2: str) -> str:
+        return f"G(({p1}) -> F({p2}))"
+
+    @staticmethod
+    def precedence(p1: str, p2: str) -> str:
+        return f"(!({p2}) U ({p1})) | G(!({p2}))"
+
+    @staticmethod
+    def chain_response(p1: str, p2: str) -> str:
+        return f"G(({p1}) -> X({p2}))"
+
+    @staticmethod
+    def chain_precedence(p1: str, p2: str) -> str:
+        return f"G(X({p2}) -> ({p1})) & !({p2})"
+
+    @staticmethod
+    def chain_succession(p1: str, p2: str) -> str:
+        return f"G(({p1}) <-> X({p2}))"
+
+    @staticmethod
+    def not_co_existence(p1: str, p2: str) -> str:
+        return f"F({p1}) -> !F({p2})"
+
+    @staticmethod
+    def not_succession(p1: str, p2: str) -> str:
+        return f"G(({p1}) -> !F({p2}))"
+
+    @staticmethod
+    def not_chain_succession(p1: str, p2: str) -> str:
+        return f"G(({p1}) -> !X({p2}))"
+
+    @staticmethod
+    def get_op_dict() -> dict:
+        return {
+            "unary": [LTLf_pattern.existence, LTLf_pattern.abscence],
+            "binary": [
+                LTLf_pattern.choice,
+                LTLf_pattern.exclusive_choice,
+                LTLf_pattern.co_existence,
+                LTLf_pattern.responded_existence,
+                LTLf_pattern.response,
+                LTLf_pattern.precedence,
+                LTLf_pattern.chain_response,
+                LTLf_pattern.chain_precedence,
+                LTLf_pattern.chain_succession,
+                LTLf_pattern.not_co_existence,
+                LTLf_pattern.not_succession,
+                LTLf_pattern.not_chain_succession,
+            ],
+        }
+
+
+mapping = {
+    PLTL_pattern.existence : LTLf_pattern.existence,
+    PLTL_pattern.abscence : LTLf_pattern.abscence,
+    PLTL_pattern.choice : LTLf_pattern.choice,
+    PLTL_pattern.exclusive_choice : LTLf_pattern.exclusive_choice,
+    PLTL_pattern.co_existence : LTLf_pattern.co_existence,
+    PLTL_pattern.responded_existence : LTLf_pattern.responded_existence,
+    PLTL_pattern.response : LTLf_pattern.response,
+    PLTL_pattern.precedence : LTLf_pattern.precedence,
+    PLTL_pattern.chain_response : LTLf_pattern.chain_response,
+    PLTL_pattern.chain_precedence : LTLf_pattern.chain_precedence,
+    PLTL_pattern.chain_succession : LTLf_pattern.chain_succession,
+    PLTL_pattern.not_co_existence : LTLf_pattern.not_co_existence,
+    PLTL_pattern.not_succession : LTLf_pattern.not_succession,
+    PLTL_pattern.not_chain_succession : LTLf_pattern.not_chain_succession
+    
+}
 
 
 def get_atoms_list(n_atoms: int) -> List[str]:
@@ -58,43 +322,57 @@ def get_atoms_list(n_atoms: int) -> List[str]:
 def create_formula(depth: int, n_atoms: int) -> str:
     atoms = get_atoms_list(n_atoms)
 
-    unary, binary = PLTL.get_op_dict().values()
+    unary_past, binary_past = PLTL_pattern.get_op_dict().values()
+    unary_future, binary_future = LTLf_pattern.get_op_dict().values()
 
-    methods_type = [round(x) + 1 for x in [random.random() for _ in range(depth)]]
+    methods_type = [2 if x > 0.2 else 1 for x in [random.random() for _ in range(depth)]]
 
-    methods = []
+    methods_past = []
+    methods_future = []
 
     for i in range(len(methods_type)):
-        methods.append(random.choice(unary if methods_type[i] == 1 else binary))
-
+        methods_past.append(random.choice(unary_past if methods_type[i] == 1 else binary_past))
+        methods_future.append(mapping[methods_past[i]])
+        
+    # from pprint import pprint as print
+    # 
+    # print(methods_type)
+    # print(methods_past)
+    # print(methods_future)
+    
     atoms_used = set()
 
     for i in range(depth):
         if i == 0:
             if methods_type[i] == 1:
                 p = random.choice(atoms)
-                formula = methods[i](p)
+                formula_past = methods_past[i](p)
+                formula_future = methods_future[i](p)
                 atoms_used.add(p)
             else:
                 p1, p2 = random.sample(atoms, 2)
-                formula = methods[i](p1, p2)
+                formula_past = methods_past[i](p1, p2)
+                formula_future = methods_future[i](p1, p2)
                 atoms_used.add(p1)
                 atoms_used.add(p2)
 
         else:
             if methods_type[i] == 1:
-                formula = methods[i](formula)
+                formula_past = methods_past[i](formula_past)
+                formula_future = methods_future[i](formula_future)
             else:
                 p = random.choice(atoms)
 
                 if random.random() < 0.5:
-                    formula = methods[i](p, formula)
+                    formula_past = methods_past[i](p, formula_past)
+                    formula_future = methods_future[i](p, formula_future)
                 else:
-                    formula = methods[i](formula, p)
+                    formula_past = methods_past[i](formula_past, p)
+                    formula_future = methods_future[i](formula_future, p )
 
                 atoms_used.add(p)
 
-    return formula, len(atoms_used)
+    return formula_past, formula_future, len(atoms_used)
 
 
 def check_sat(formula):
@@ -103,8 +381,8 @@ def check_sat(formula):
 
 def write_to_json(formulas, dir, filename):
     res = {}
-    for i, (formula, atoms) in enumerate(formulas):
-        res[f"f{i}"] = {"formula": formula, "n_atoms": atoms}
+    for i, (formula_past, formula_future, atoms) in enumerate(formulas):
+        res[f"f{i}"] = {"formula_past": formula_past, "formula_future": formula_future,"n_atoms": atoms}
 
     with open(os.path.join(dir, f"{filename}.json"), "w") as ofile:
         ofile.write(json.dumps(res))
@@ -122,10 +400,10 @@ def main(args):
 
     while n_formulas < args["number"]:
 
-        formula, n_atoms = create_formula(args["depth"], args["prop_atom"])
-      
-        if check_sat(formula):
-            generated_formulas.append((formula, n_atoms))
+        formula_past, formula_future, n_atoms = create_formula(args["depth"], args["prop_atom"])
+
+        if check_sat(formula_past):
+            generated_formulas.append((formula_past, formula_future, n_atoms))
             n_formulas += 1
 
     write_to_json(generated_formulas, args["directory"], args["filename"])
